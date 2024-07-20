7b4a3ea106606bd07dc543d840c9e1ac62944203
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-20 06:38:44 -0400

    Update upload_iso_to_proxmox.sh

--- Setup/upload_iso_to_proxmox.sh
+++ Setup/upload_iso_to_proxmox.sh
@@ -24,22 +24,22 @@ upload_iso_to_proxmox() {
 
   # Upload Windows 10 ISO
   log "Uploading Windows 10 ISO..."
-  sshpass -p "$PROXMOX_PASS" scp ansible/downloads/windows_10_ISO/windows10.iso $PROXMOX_USER@$PROXMOX_IP:$ISO_DIR
+  scp ansible/downloads/windows_10_ISO/windows10.iso $PROXMOX_USER@$PROXMOX_IP:$ISO_DIR
   log "Windows 10 ISO uploaded."
 
   # Upload Windows Server 2019 ISO
   log "Uploading Windows Server 2019 ISO..."
-  sshpass -p "$PROXMOX_PASS" scp ansible/downloads/windows_server_2019_ISO/windows_server_2019.iso $PROXMOX_USER@$PROXMOX_IP:$ISO_DIR
+  scp ansible/downloads/windows_server_2019_ISO/windows_server_2019.iso $PROXMOX_USER@$PROXMOX_IP:$ISO_DIR
   log "Windows Server 2019 ISO uploaded."
 
   # Upload Snare Central VMA
   log "Uploading Snare Central VMA..."
-  sshpass -p "$PROXMOX_PASS" scp ansible/downloads/snare_central_ISO/Snare_Central.vma.zst $PROXMOX_USER@$PROXMOX_IP:$DUMP_DIR
+  scp ansible/downloads/snare_central_ISO/Snare_Central.vma.zst $PROXMOX_USER@$PROXMOX_IP:$DUMP_DIR
   log "Snare Central VMA uploaded."
 
   # Upload VirtIO ISO
   log "Uploading VirtIO ISO..."
-  sshpass -p "$PROXMOX_PASS" scp ansible/downloads/virtio-win.iso $PROXMOX_USER@$PROXMOX_IP:$ISO_DIR
+  scp ansible/downloads/virtio-win.iso $PROXMOX_USER@$PROXMOX_IP:$ISO_DIR
   log "VirtIO ISO uploaded."
 
   log "All ISO files uploaded successfully."
@@ -52,7 +52,7 @@ main() {
   echo -e "\033[1;34m
   ##############################################################
   #                                                            #
-  #    NEXT STEP: Setup complete.                              #
+  #    Setup complete.                                         #
   #                                                            #
   ##############################################################
   \033[0m"
