5962d33c102cfcb645e63cc873405347006bca8a
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-24 11:59:29 -0400

    Update prepare_project.sh

--- prepare_project.sh
+++ prepare_project.sh
@@ -7,18 +7,68 @@ log() {
   echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOGFILE
 }
 
+error_exit() {
+  log "ERROR: $1"
+  exit 1
+}
+
+update_system_and_install_dependencies() {
+  echo -e "\n\n####################### Starting Setup #######################\n" | tee -a $LOGFILE
+
+  log "Updating and upgrading the system, and installing required packages..."
+
+  sudo apt update && sudo apt upgrade -y
+  sudo apt install -y git gpg nano tmux curl gnupg software-properties-common mkisofs python3-venv python3 python3-pip unzip mono-complete coreutils whiptail pv sshpass
+
+  log "System update and installation of dependencies completed."
+}
+
+mkdir Git_Project
+cd Git_Project
+git clone https://github.com/VeteranTechSolutions/Snare_Lab_POC.git
+
+log "Installing python3-venv..."
+sudo apt install -y python3-venv || error_exit "Failed to install python3-venv."
+log "python3-venv installed successfully."
+
+log "Creating Python virtual environment..."
+python3 -m venv venv || error_exit "Failed to create Python virtual environment."
+log "Python virtual environment created successfully."
+
 prepare_project() {
   log "Making project scripts executable..."
-  chmod +x create_venv.sh
-  chmod +x source_venv.sh
-  chmod +x Setup/update_system_and_install_dependencies.sh
-  chmod +x Setup/configure_user_and_replace_placeholders.sh
-  chmod +x Setup/download_iso_files.sh
-  chmod +x Setup/download_snare_files.sh
-  chmod +x Setup/install_automation_tools.sh
-  chmod +x Setup/reassemble_iso_files.sh
- #chmod +x Setup/upload_iso.sh 
+  chmod +x ./Snare_Lab_POC/create_venv.sh
+  chmod +x ./Snare_Lab_POC/Setup/update_system_and_install_dependencies.sh
+  chmod +x ./Snare_Lab_POC/Setup/configure_user_and_replace_placeholders.sh
+  chmod +x ./Snare_Lab_POC/Setup/download_iso_files.sh
+  chmod +x ./Snare_Lab_POC/Setup/download_snare_files.sh
+  chmod +x ./Snare_Lab_POC/Setup/install_automation_tools.sh
+  chmod +x ./Snare_Lab_POC/Setup/reassemble_iso_files.sh
+  chmod +x ./Snare_Lab_POC/Setup/upload_iso.sh
 
-  log "Running create_venv.sh..."
-  source ./create_venv.sh
+  echo -e "\033[1;34m
+  ########################################################
+  #                                                      #
+  #                STEP 1 COMPLETE                       #
+  #                                                      #
+  ########################################################
+  \033[0m"
 
+  echo -e "\033[1;34m
+  ##############################################################
+  #                                                            #
+  #    NEXT STEP: Run the following commands:                  #
+  #                                                            #
+  #    source venv/bin/activate                                #
+  #    ./configure_user_and_replace_placeholders.sh            #
+  #                                                            #
+  ##############################################################
+  \033[0m"
+}
+
+main() {
+  update_system_and_install_dependencies
+  prepare_project
+}
+
+main
