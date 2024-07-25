f2cc6dcb6a04c77c2f81beae54a96243914a2588
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-25 17:08:23 -0400

    Update configure_user_and_replace_placeholders.sh

--- Setup/configure_user_and_replace_placeholders.sh
+++ Setup/configure_user_and_replace_placeholders.sh
@@ -64,6 +64,7 @@ EOF
   echo "PROXMOX_API_TOKEN=$API_TOKEN" >> ../.env
   echo "PROXMOX_NODE_IP=$PROXMOX_IP" >> ../.env
   echo "PROXMOX_NODE_NAME=$PROXMOX_NODE_NAME" >> ../.env
+  echo "PROXMOX_PASSWORD=$PROXMOX_PASSWORD" >> ../.env
   log ".env file created successfully with the captured API token."
 
   echo -e "\033[1;32m
