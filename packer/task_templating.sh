45aa6b2ab5d2375146e55b29b3b59ab1c87ed326
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-25 15:42:05 -0400

    Update task_templating.sh

--- packer/task_templating.sh
+++ packer/task_templating.sh
@@ -13,6 +13,15 @@ create_templates(){
     
 }
 
+#run_next_script() {
+#  log "AUTOMATICALLY RUNNING THE NEXT SCRIPT task_terraforming.sh"
+#  cd ~/Git_Project/Snare_Lab_POC/terraform
+#  ./task_terraforming.sh
+#}
+
+
 create_templates
+#run_next_script
+
 
 echo "[+] run the task_terraforming.sh in terraform/"
