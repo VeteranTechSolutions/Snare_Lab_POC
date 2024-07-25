9bd2279be307aee4303da6bfae5e16e5e7e2645b
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-25 15:21:50 -0400

    Update reassemble_snare_central.iso.sh

--- ansible/images/snare_central_iso/reassemble_snare_central.iso.sh
+++ ansible/images/snare_central_iso/reassemble_snare_central.iso.sh
@@ -2,15 +2,15 @@
 
 # Reassemble the parts into a single file
 echo "Reassembling the parts..."
-cat Snare_Central.vma.zst.part* > "snare_central.iso"
+cat Snare_Central.vma.zst.part* > "snare_central.vma.zst"
 
 # List the reassembled file to verify
 echo "Reassembled file:"
-ls -lh "snare_central.iso"
+ls -lh "snare_central.vma.zst"
 
 # Generate checksums for the reassembled file
 echo "Generating checksum for the reassembled file..."
-shasum -a 256 "snare_central.iso" > reassembled_checksum.txt
+shasum -a 256 "snare_central.vma.zst" > reassembled_checksum.txt
 
 # Compare the checksums
 echo "Comparing checksums..."
