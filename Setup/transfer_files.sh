7d5b7edcf6a0a70ce03716845e222596b2b906d2
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-25 15:04:33 -0400

    Create transfer_files.sh

---
+++ Setup/transfer_files.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+LOGFILE=setup.log
+
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOGFILE
+}
+
+error_exit() {
+  log "ERROR: $1"
+  exit 1
+}
+
+source_env() {
+  SSHENV_PATH=~/Git_Project/Snare_Lab_POC/SSHENV
+  if [ -f $SSHENV_PATH ]; then
+    log "Sourcing SSHENV file..."
+    source $SSHENV_PATH
+  else
+    error_exit "SSHENV file not found at $SSHENV_PATH! Exiting..."
+  fi
+}
+
+transfer_files() {
+  echo -e "\n\n####################### Starting File Transfer #######################\n" | tee -a $LOGFILE
+
+  source_env
+
+  log "Transferring local files to Proxmox server..."
+
+  LOCAL_FILES=(
+    "~/Git_Project/Snare_Lab_POC/ansible/images/windows_server_2019_ISO/windows_server_2019.iso"
+    "~/Git_Project/Snare_Lab_POC/ansible/images/windows_10_iso/windows_10.iso"
+    "~/Git_Project/Snare_Lab_POC/ansible/images/snare_central_iso/snare_central.vma.zst"
+  )
+
+  REMOTE_PATHS=(
+    "/var/lib/vz/template/iso/windows_server_2019.iso"
+    "/var/lib/vz/template/iso/windows_10.iso"
+    "/var/lib/vz/dump/snare_central.vma.zst"
+  )
+
+  for index in "${!LOCAL_FILES[@]}"; do
+    LOCAL_FILE=${LOCAL_FILES[$index]}
+    REMOTE_PATH=${REMOTE_PATHS[$index]}
+    log "Transferring $LOCAL_FILE to $REMOTE_PATH on Proxmox server..."
+
+    sshpass -p "$PROXMOX_PASSWORD" scp $LOCAL_FILE $PROXMOX_USER@$PROXMOX_IP:$REMOTE_PATH >> $LOGFILE 2>&1
+
+    if [ $? -ne 0 ]; then
+      error_exit "Failed to transfer $LOCAL_FILE to $REMOTE_PATH on Proxmox server."
+    else
+      log "$LOCAL_FILE transferred successfully to $REMOTE_PATH."
+    fi
+  done
+
+  echo -e "\033[1;32m
+  ##############################################################
+  #                                                            #
+  #    File transfer completed successfully.                   #
+  #                                                            #
+  ##############################################################
+  \033[0m"
+}
+
+run_next_script() {
+  log "AUTOMATICALLY RUNNING THE NEXT SCRIPT your_next_script.sh"
+  cd ~/Git_Project/Snare_Lab_POC/Setup
+  ./your_next_script.sh
+}
+
+transfer_files
+run_next_script
