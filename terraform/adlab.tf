d6a9c27eb897718d458dde67b92256cdfc6eb161
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-26 10:41:04 -0400

    Update adlab.tf

--- terraform/adlab.tf
+++ terraform/adlab.tf
@@ -10,9 +10,6 @@ provider "proxmox" {
   endpoint  = "https://${var.pve_node_ip}:8006/api2/json"
   api_token = "${var.tokenid}=${var.tokenkey}"
   insecure  = true
-  ssh {
-    agent    = true
-  }
 }
 
 resource "proxmox_virtual_environment_pool" "training_pool" {
@@ -21,19 +18,19 @@ resource "proxmox_virtual_environment_pool" "training_pool" {
 }
 
 data "proxmox_virtual_environment_vms" "sc" {
-  tags      = ["traininglab-sc"]
+  tags = ["traininglab-sc"]
 }
 
 data "proxmox_virtual_environment_vms" "ubuntu_server" {
-  tags      = ["traininglab-server"]
+  tags = ["traininglab-server"]
 }
 
 data "proxmox_virtual_environment_vms" "win2019" {
-  tags      = ["traininglab-win2019"]
+  tags = ["traininglab-win2019"]
 }
 
 data "proxmox_virtual_environment_vms" "ws" {
-  tags      = ["traininglab-ws"]
+  tags = ["traininglab-ws"]
 }
 
 locals {
@@ -52,42 +49,164 @@ locals {
   }
 }
 
-resource "proxmox_virtual_environment_vm" "vm" {
-  for_each = {
-    Snare-Central = local.vm_id_templates.snare_central
-    Linux-Desktop = local.vm_id_templates.ubuntu_server
-    Win-Server         = local.vm_id_templates.windows_server_2019
-    Win-Desktop        = local.vm_id_templates.windows_10
-  }
-
-  name = each.key
-  pool_id = proxmox_virtual_environment_pool.training_pool.pool_id
+resource "proxmox_virtual_environment_vm" "snare_central" {
+  name      = "Snare-Central"
+  pool_id   = proxmox_virtual_environment_pool.training_pool.pool_id
   node_name = var.pve_node
-  on_boot = false
+  on_boot   = false
 
   clone {
-    vm_id = each.value
-    full = false
+    vm_id  = local.vm_id_templates.snare_central
+    full   = false
     retries = 2
   }
 
   agent {
     enabled = true
-  } 
+  }
 
   memory {
-    dedicated = each.key == "ubuntu" ? 2048 : local.default_vm_config.memory
+    dedicated = 16000
   }
 
   cpu {
-    cores       = local.default_vm_config.cores
-    sockets     = local.default_vm_config.sockets
+    cores   = 4
+    sockets = 1
   }
 
   disk {
     interface    = "scsi0"
-    file_format = "raw"
-    size    = local.default_vm_config.disk_size
+    file_format  = "raw"
+    size         = "500GB"
+    datastore_id = var.storage_name
+  }
+
+  network_device {
+    bridge = var.netbridge
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disk,
+    ]
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "ubuntu_server" {
+  name      = "Linux-Desktop"
+  pool_id   = proxmox_virtual_environment_pool.training_pool.pool_id
+  node_name = var.pve_node
+  on_boot   = false
+
+  clone {
+    vm_id  = local.vm_id_templates.ubuntu_server
+    full   = false
+    retries = 2
+  }
+
+  agent {
+    enabled = true
+  }
+
+  memory {
+    dedicated = local.default_vm_config.memory
+  }
+
+  cpu {
+    cores   = local.default_vm_config.cores
+    sockets = local.default_vm_config.sockets
+  }
+
+  disk {
+    interface    = "scsi0"
+    file_format  = "raw"
+    size         = local.default_vm_config.disk_size
+    datastore_id = var.storage_name
+  }
+
+  network_device {
+    bridge = var.netbridge
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disk,
+    ]
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "win_server" {
+  name      = "Win-Server"
+  pool_id   = proxmox_virtual_environment_pool.training_pool.pool_id
+  node_name = var.pve_node
+  on_boot   = false
+
+  clone {
+    vm_id  = local.vm_id_templates.windows_server_2019
+    full   = false
+    retries = 2
+  }
+
+  agent {
+    enabled = true
+  }
+
+  memory {
+    dedicated = local.default_vm_config.memory
+  }
+
+  cpu {
+    cores   = local.default_vm_config.cores
+    sockets = local.default_vm_config.sockets
+  }
+
+  disk {
+    interface    = "scsi0"
+    file_format  = "raw"
+    size         = local.default_vm_config.disk_size
+    datastore_id = var.storage_name
+  }
+
+  network_device {
+    bridge = var.netbridge
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disk,
+    ]
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "win_desktop" {
+  name      = "Win-Desktop"
+  pool_id   = proxmox_virtual_environment_pool.training_pool.pool_id
+  node_name = var.pve_node
+  on_boot   = false
+
+  clone {
+    vm_id  = local.vm_id_templates.windows_10
+    full   = false
+    retries = 2
+  }
+
+  agent {
+    enabled = true
+  }
+
+  memory {
+    dedicated = local.default_vm_config.memory
+  }
+
+  cpu {
+    cores   = local.default_vm_config.cores
+    sockets = local.default_vm_config.sockets
+  }
+
+  disk {
+    interface    = "scsi0"
+    file_format  = "raw"
+    size         = local.default_vm_config.disk_size
     datastore_id = var.storage_name
   }
 
@@ -107,14 +226,14 @@ resource "proxmox_virtual_environment_vm" "vm" {
 output "ansible_inventory" {
   value = templatefile("${path.module}/inventory_hosts.tmpl", {
     linux_ips = {
-      "Linux-Desktop" = proxmox_virtual_environment_vm.vm["Linux-Desktop"].ipv4_addresses[1][0]
+      "Linux-Desktop" = proxmox_virtual_environment_vm.ubuntu_server.ipv4_addresses[1][0]
     },
     snare_central_ips = {
-      "Snare-Central" = proxmox_virtual_environment_vm.vm["Snare-Central"].ipv4_addresses[1][0]
+      "Snare-Central" = proxmox_virtual_environment_vm.snare_central.ipv4_addresses[1][0]
     },
     windows_ips = {
-      "Win-Server"  = proxmox_virtual_environment_vm.vm["Win-Server"].ipv4_addresses[0][0]
-      "Win-Desktop" = proxmox_virtual_environment_vm.vm["Win-Desktop"].ipv4_addresses[0][0]
+      "Win-Server"  = proxmox_virtual_environment_vm.win_server.ipv4_addresses[0][0]
+      "Win-Desktop" = proxmox_virtual_environment_vm.win_desktop.ipv4_addresses[0][0]
     }
   })
 }
