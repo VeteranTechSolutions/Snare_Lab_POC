94e086c6200d1f6a25adeb77ef43012e0d93e79d
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-30 11:21:36 -0400

    Update download_iso_files.sh

--- Setup/download_iso_files.sh
+++ Setup/download_iso_files.sh
@@ -22,42 +22,42 @@ download_iso_files() {
   cd ~/Git_Project/Snare_Lab_POC/ansible || error_exit "Failed to change directory to ~/Git_Project/Snare_Lab_POC/ansible/"
 
   FILES=(
-    "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/ubuntu-22.iso"
+    #"https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/ubuntu-22.iso"
     "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/virtio-win.iso"
-    "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows10.iso.partaa"
-    "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows10.iso.partab"
-    "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows10.iso.partac"
+    #"https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows10.iso.partaa"
+    #"https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows10.iso.partab"
+    #"https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows10.iso.partac"
     "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/snare_central.vma.zst.partaa"
     "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/snare_central.vma.zst.partab"
-    "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows_server_2019.iso.partaa"
-    "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows_server_2019.iso.partab"
-    "https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows_server_2019.iso.partac"
+    #"https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows_server_2019.iso.partaa"
+    #"https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows_server_2019.iso.partab"
+    #"https://github.com/VeteranTechSolutions/Snare_Lab_POC/releases/download/POC_downloads/windows_server_2019.iso.partac"
   )
 
   FILENAMES=(
-    "ubuntu-22.iso"
+    #"ubuntu-22.iso"
     "virtio-win.iso"
-    "windows10.iso.partaa"
-    "windows10.iso.partab"
+    #"windows10.iso.partaa"
+    #"windows10.iso.partab"
     "windows10.iso.partac"
     "snare_central.vma.zst.partaa"
     "snare_central.vma.zst.partab"
-    "windows_server_2019.iso.partaa"
-    "windows_server_2019.iso.partab"
-    "windows_server_2019.iso.partac"
+    #"windows_server_2019.iso.partaa"
+    #"windows_server_2019.iso.partab"
+    #"windows_server_2019.iso.partac"
   )
 
   DIRECTORIES=(
-    "images/ubuntu_22_iso"
+    #"images/ubuntu_22_iso"
     "images"
-    "images/windows_10_iso"
-    "images/windows_10_iso"
-    "images/windows_10_iso"
+    #"images/windows_10_iso"
+    #"images/windows_10_iso"
+    #"images/windows_10_iso"
     "images/snare_central_iso"
     "images/snare_central_iso"
-    "images/windows_server_2019_iso"
-    "images/windows_server_2019_iso"
-    "images/windows_server_2019_iso"
+    #"images/windows_server_2019_iso"
+    #"images/windows_server_2019_iso"
+    #"images/windows_server_2019_iso"
   )
 
   for index in "${!FILES[@]}"; do
